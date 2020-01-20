04126bad586028bfe22c5edce74aa085166d3ad7
Tjebbe <tjebbemarchand@icloud.com>
2020-01-20 15:43:40 +0100

    Added speak functionality

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Speech API.
 const speechApi = window.speechSynthesis;
 
-// DOM Elements.
 const textForm = document.querySelector("form");
 const textInput = document.querySelector("#text-input");
 const voiceSelect = document.querySelector("#voice-select");
@@ -10,14 +9,12 @@ const rateValue = document.querySelector("#rate-value");
 const pitch = document.querySelector("#pitch");
 const pitchValue = document.querySelector("#pitch-value");
 
-// Init voices array.
 let voices = null;
 
 function getVoices() {
     voices = speechApi.getVoices();
 
     voices.forEach(voice => {
-        // Create option element.
         const option = document.createElement("option");
         option.textContent = `${voice.name} - (${voice.lang})`;
         option.setAttribute("data-lang", voice.lang);
@@ -27,7 +24,48 @@ function getVoices() {
 }
 
 getVoices();
-
 if (speechApi.onvoiceschanged !== undefined) {
     speechApi.onvoiceschanged = getVoices;
 }
+
+function speak() {
+    if (speechApi.speaking) {
+        console.error("Already speaking...");
+        return;
+    }
+
+    if (textInput.value === "") {
+        console.error("Input empty...");
+        return;
+    }
+
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
+    speakText.onend = e => {
+        console.log("Done speaking...");
+    };
+
+    speakText.onerror = e => {
+        console.error("Something went wrong...");
+    };
+
+    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute("data-name");
+    voices.forEach(voice => {
+        if (voice.name === selectedVoice) {
+            speakText.voice = voice;
+        }
+    });
+
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+
+    speechApi.speak(speakText);
+}
+
+textForm.addEventListener("submit", e => {
+    e.preventDefault();
+    speak();
+    textInput.blur();
+});
+
+rate.addEventListener("change", () => (rateValue.textContent = rate.value));
+pitch.addEventListener("change", () => (pitchValue.textContent = pitch.value));
